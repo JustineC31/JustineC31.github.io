9c1644a7352c10dac0e521140c276f30cb405ab7
JustineC31 <justine-cousin@hotmail.com>
2024-10-03 14:47:08 +0200

    Initial commit

---
+++ script.js
@@ -0,0 +1,4 @@
+// Ce script peut être utilisé pour ajouter des fonctionnalités interactives
+document.addEventListener('DOMContentLoaded', () => {
+    console.log("Le site est chargé et prêt !");
+});
